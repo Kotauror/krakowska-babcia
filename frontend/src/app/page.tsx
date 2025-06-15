50787963268b10a7f2a509e80da49a52ca67d0fc
Kotauror <zygmunt.justyna@gmail.com>
2025-06-15 20:11:43 +0200

    styling changes.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
 
           <section className="p-8 bg-gray-50">
             <h2 className="text-3xl font-bold mb-6">Wycieczki</h2>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 pb-8">
               {posts
                 ?.filter((post) => post.id !== latestPost?.id)
                 .map((post: Post) => (
