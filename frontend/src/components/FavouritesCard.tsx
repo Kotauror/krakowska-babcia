fc6baca07ca3ced7c0ef0aee328d15e524f44fdf
Kotauror <zygmunt.justyna@gmail.com>
2025-10-05 21:09:48 +0200

    fav card style'

--- frontend/src/components/FavouritesCard.tsx
+++ frontend/src/components/FavouritesCard.tsx
@@ -19,25 +19,16 @@ function FavouritesCard({ post }: { post: Post }) {
 
   return (
     <Link href={`/posts/${slugify(post.tytul)}?id=${post.id}`} className="mt-4 inline-block">
-      <article className="rounded-lg shadow-lg overflow-hidden border-1 border-gray-400 hover:scale-105 transition-all duration-300 bg-[#fff6e7]">
-        <div className="w-[300px] h-[200px] mx-auto overflow-hidden">
-          {/* {firstImageUrl && !imageError ? ( */}
-            <img
-              src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}assets/${post.zdjecie_glowne}`}
-              alt={post.tytul}
-              className="w-full h-full object-cover object-center"
-              // onError={() => setImageError(true)}
-            />
-          {/* ) : ( */}
-            {/* <img
-              src="/icon.png"
-              alt="Default icon"
-              className="w-full h-full object-contain p-4"
-            /> */}
-          {/* )} */}
+      <article className="rounded-lg shadow-lg overflow-hidden border-1 border-gray-400 hover:scale-105 transition-all duration-300 bg-[#fff6e7] h-[300px] flex flex-col">
+        <div className="w-[300px] h-[200px] mx-auto overflow-hidden flex-shrink-0">
+          <img
+            src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}assets/${post.zdjecie_glowne}`}
+            alt={post.tytul}
+            className="w-full h-full object-cover object-center"
+          />
         </div>
-        <div className="p-6">
-          <h3 className="md:text-lg text-base mb-2">{post.tytul}</h3>
+        <div className="p-6 flex flex-col justify-center items-center text-center flex-grow">
+          <h3 className="md:text-lg text-base mb-2 leading-tight line-clamp-2 overflow-hidden">{post.tytul}</h3>
           <div className="text-gray-600 mb-4 text-sm">
           </div>
         </div>
